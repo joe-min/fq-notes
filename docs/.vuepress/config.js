b6ea06e5f1328069bc901b187bcc172f13e4b2a0
fengqiao <384147053@qq.com>
2023-11-23 20:03:08 +0800

    初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,30 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "fq-notes",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/fq-notes/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "d3ec4ca6363950ca41a2",
+        clientSecret: "897465b6393f1d663e6128d2fab6959a0c0333cc",
+        owner: "lql95",
+        repo: "lql-notes",
+      },
+    ],
+  ],
   themeConfig,
 }
